96f35ad1737ff3e7d877244411ed1329b716b726
Rishat-F <nilluziaf@gmail.com>
2021-06-21 13:22:50 +0300

    Created MainPageLocators class and refactored

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,16 @@
 from .pages.main_page import MainPage
 
 
+link = 'http://selenium1py.pythonanywhere.com/'
+
+
 def test_guest_should_see_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)  # initializing Page Object
     page.open()                     # page opening
     page.should_be_login_link()     # checks is there login link on page
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
